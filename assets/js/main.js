eae27d9a43fd266b0f733268c620c560ad44d970
Tyler Nickerson <nickersoft@gmail.com>
2015-05-06 22:08:45 -0400

    Added socket.io room functionality and allowed users to disconnect (changes not fully reflected on the client side)

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,12 @@
 var socket = io();
-var userid, chatid;
-
-$(window).bind("beforeunload", function() {
-    socket.emit('leave', {
-        user: userid
-    });
-});
+var userid;
 
 $('#send_form').submit(function() {
     socket.emit('message', {
-        "uid" : userid,
-        "cid" : chatid, 
+        "user" : userid,
         "msg" : $("#send_box").val()
     });
-
+    
 	$("#send_box").val('');
 	return false;
 });
@@ -22,32 +15,29 @@ $("#send_button").click(function() {
 	$("#send_form").submit();
 });
 
-socket.on('message', function(data) {
-	if(data["cid"] == chatid) {
-        var prepend = "Stranger";
-        var css_class = "them";
-        if(data["uid"] == userid) {
-            prepend = "Me";
-            css_class = "me"
-        }
-        $('#chat_messages ul').eq(0).append($('<li>').addClass(css_class).html("<span>" + prepend + ": </span>" + data["msg"]).fadeIn());   
-	}
+socket.on('boot', function(data) {
+    $('#chat_messages ul').eq(0).append($('<li>').html("Your chat partner has disconnected.").fadeIn());  
 });
 
-socket.on('enter', function(data) {
-    if(userid == undefined) {
-        userid = data["user"];
-        console.log("Running as user " + userid);
-        socket.emit('request', { "uid" : userid });
-        $("#send_box").attr('placeholder', 'Waiting for a chat partner...').prop('disabled', true);   
+socket.on('message', function(data) {
+    var prepend = "Stranger";
+    var css_class = "them";
+    console.log(data);
+    if(data["user"] == userid) {
+        prepend = "Me";
+        css_class = "me"
     }
+    $('#chat_messages ul').eq(0).append($('<li>').addClass(css_class).html("<span>" + prepend + ": </span>" + data["msg"]).fadeIn()); 
 });
 
 socket.on('assign', function(data) {
-   if(data["user"] == userid) {
-       chatid = data["chat"];
-       console.log("Currently in chat " + chatid + " with user " + data["partner"]);
-       $("#send_box").attr('placeholder', 'Click here to start typing').prop('disabled', false);
-       $("#send_button").removeClass('uk-icon-spin uk-icon-circle-o-notch').addClass('uk-icon-send');
-   }
+    userid = data["user"];
+    socket.emit('request');
+    $("#send_box").attr('placeholder', 'Waiting for a chat partner...').prop('disabled', true);   
+});
+
+socket.on('connected', function(data) {
+   console.log("Currently in chat with user");
+   $("#send_box").attr('placeholder', 'Click here to start typing').prop('disabled', false);
+   $("#send_button").removeClass('uk-icon-spin uk-icon-circle-o-notch').addClass('uk-icon-send');
 });
--- server.js
+++ server.js
@@ -3,6 +3,9 @@ var app  	= express();
 var http 	= require('http').Server(app);
 var io	 	= require('socket.io')(http);
 
+var Chance  = require('chance'),
+chance = new Chance();
+
 app.use('/css', express.static(__dirname + '/assets/css/core'));
 app.use('/js', express.static(__dirname + '/assets/js/'));
 app.use('/images', express.static(__dirname + '/assets/img'));
@@ -18,75 +21,53 @@ app.get('/', function(req, res) {
 
 var homeless = [];
 
-var guid = (function() {
-    
-    function s4() {
-        return Math.floor((1 + Math.random()) * 0x10000)
-                .toString(16)
-                .substring(1);
-    }
-    
-    return function() {
-        return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
-            s4() + '-' + s4() + s4() + s4();
-    };
-    
-})();
-
 var removeElement = function(array, element) {
     for(var i = 0; i < array.length; i++) {
-        if(array[i] == element) {
+        if(array[i].username == element.username) {
             array.splice(i, 1);
         }
     }
 };
 
-var matchUser = function(user) {
-
-    removeElement(homeless, user);
-    
-    var next = homeless.shift();
-    var chat = guid();
-    
-    io.emit('assign', { 
-        user : user, 
-        chat : chat, 
-        partner : next 
-    });
-
-    io.emit('assign', { 
-        user : next, 
-        chat : chat, 
-        partner : user
-    });
-    
-    console.log("User " + user + " has entered a chat with " + next);
-};
-
 io.on('connection', function(socket) {
-    var new_user = guid();
-    io.emit('enter', { user : new_user });
-    console.log("User " + new_user + " has logged on");
+    var new_user = chance.guid();
+    socket.username = new_user;
+    socket.emit('assign', { "user" : new_user });
+    console.log("User " + socket.username + " has logged on");
     
 	socket.on('message', function(data) {
-		io.emit('message', data);
+        io.in(socket.room).emit('message', data);
 	});
     
     socket.on('request', function(data) {
-        if(data.hasOwnProperty("uid")) {
-            console.log("User " + data["uid"] + " is currently available to chat");
-            homeless.push(data["uid"]);
-            if(homeless.length > 1) {
-                matchUser(data["uid"]);
-            }
+        console.log("User " + socket.username + " is currently available to chat");
+        homeless.push(socket);
+        if(homeless.length > 1) {
+            
+            removeElement(homeless, socket);
+    
+            var next = homeless.shift();
+            var chat = chance.guid();
+            
+            socket.join(chat);
+            socket.room = chat;
+            socket.emit('connected');
+            
+            next.join(chat);
+            next.room = chat;
+            next.emit('connected');
+            
+            console.log("User " + socket.username + " has entered a chat with " + next.username + " in room " + socket.room);
         }
 	});
     
-    socket.on('leave', function(data) {
-        if(data.hasOwnProperty("uid")) {
-            var uid = data["user"];
-            console.log("User " + uid + " has left");
+    socket.on('disconnect', function(data) {
+        if(socket.room !== undefined) {
+            io.in(socket.room).emit('boot');
+        } else {
+            removeElement(homeless, socket);
         }
+        console.log("User " + socket.username + " has logged off");
     });
 });
 
