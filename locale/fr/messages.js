2fde54b727eb2f1109b141b0784dc48ba3a57ef3
Tyler Nickerson <nickersoft@gmail.com>
2015-05-31 13:04:51 -0400

    Added language support for French

---
+++ locale/fr/messages.js
@@ -0,0 +1,76 @@
+;var json_locale_data = {
+   "messages": {
+      "": {
+         "Project-Id-Version": " PACKAGE VERSION\nLanguage-Team: French\nReport-Msgid-Bugs-To: \nPO-Revision-Date: 2015-05-31 12:45-0400\nLanguage: fr\nMIME-Version: 1.0\nContent-Type: text/plain; charset=UTF-8\nContent-Transfer-Encoding: 8bit\nPOT-Creation-Date: 2015-05-30 02:48+0000\nLast-Translator: Tyler Nickerson <nickersoft@gmail.com>\nPlural-Forms: nplurals=2; plural=(n > 1);\n"
+      },
+      "Me": [
+         null,
+         "Me"
+      ],
+      "Stranger": [
+         null,
+         "Partenaire"
+      ],
+      "Click here to start typing (press 'Esc' to disconnect)": [
+         null,
+         "Cliquez ici pour commencer a taper (appuyez sur “esc” pour déconnecter)"
+      ],
+      "Waiting for a chat partner...": [
+         null,
+         "En attende d’un partenaire de chat..."
+      ],
+      "Start a new chat": [
+         null,
+         "Démarrer une nouvelle discussion"
+      ],
+      "You are now talking to a native speaker somewhere in the world": [
+         null,
+         "Vous parlez maintenant d’un locuteur natif, quelque part dans le monde"
+      ],
+      "You have disconnected": [
+         null,
+         "Vous avez déconnecté"
+      ],
+      "Your chat partner has disconnected": [
+         null,
+         "Votre partenaire de chat a déconnecté"
+      ],
+      "Your partner is typing...": [
+         null,
+         "Votre partenaire est attachant..."
+      ],
+      "You cannot learn your native language": [
+         null,
+         "Vous ne pouvez pas apprendre votre langue maternelle"
+      ],
+      "Please select a language for each category first": [
+         null,
+         "S’il vous plaît sélectionner une langue pour chaque catégorie premier"
+      ],
+      "people online": [
+         null,
+         " personnes en ligne"
+      ],
+      "Translate": [
+         null,
+         "Traduire"
+      ],
+      "Site Title": [
+         null,
+         "Linguiste - Parler Au Monde"
+      ],
+      "Which language do you speak natively?": [
+         null,
+         "Quelle langue vous parlez faire nativement?"
+      ],
+      "Which language are you currently studying?": [
+         null,
+         "Quelle langue vous étudiez en ce moment?"
+      ],
+      "Enter": [
+         null,
+         "Entrer"
+      ]
+   }
+}
+;
