723e45f2abce5f146ff5336e607d90bfa10aecc4
Tyler Nickerson <nickersoft@gmail.com>
2015-05-31 13:24:07 -0400

    Fixed an issue where the English language files were not be located correctly

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var Chance = require('chance'),
 // Preconfig
 app.use('/', express.static(__dirname));
 app.use(i18n.abide({
-    supported_languages: ['en_US', 'fr', 'zh'],
+    supported_languages: ['en-US', 'fr', 'zh'],
     default_lang: 'fr',
     translation_directory: 'locale',
 }));
